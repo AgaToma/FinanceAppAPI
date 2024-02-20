1a8902d18e581e9b50e9b29feefc740211c4d064
Aga Toma <mugoimajo@gmail.com>
2024-02-20 22:13:19 +0000

    add update and delete stocks with dtos

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a605301574564dca339d64bc2121467563082a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ab98ce5ce7c71e0e6081ec8f2c32687f2dbbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
