02b0755cfb9d1fba33cd6a0aabed33f6160868ee
Aga Toma <mugoimajo@gmail.com>
2024-02-21 12:00:59 +0000

    refactor stock controller with async

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ab98ce5ce7c71e0e6081ec8f2c32687f2dbbe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8902d18e581e9b50e9b29feefc740211c4d064")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
