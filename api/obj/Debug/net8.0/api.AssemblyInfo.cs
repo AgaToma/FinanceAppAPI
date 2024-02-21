2ed53494594e32f0cffb098aee2fcdb296f2c89c
Aga Toma <mugoimajo@gmail.com>
2024-02-21 13:25:31 +0000

    refactor remaining controller functions with repo

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b0755cfb9d1fba33cd6a0aabed33f6160868ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6d669a48581bea5269b6be659bcd9b8d3b9840")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
