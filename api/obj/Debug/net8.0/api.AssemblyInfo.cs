f8b60188ae2ad00bc53e64c2819b0dc7cb6954a4
Aga Toma <mugoimajo@gmail.com>
2024-02-26 21:57:26 +0000

    add register and login api Services

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c49e5cf0b5306f89e1da97ec870b041c6edb92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96fb5910b0b904928ebe9713ecd8e0a7d71135c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
